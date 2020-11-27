5da9f069a654ac3b91865a898b89fc9492884d16
joxda <joxda@yahoo.de>
2020-11-27 21:37:00 +0200

    fix embed in macos

--- j3AstroIO.cpp
+++ j3AstroIO.cpp
@@ -73,12 +73,11 @@ PhotoPars getPars(const char* file)
     {
         Exiv2::XmpParser::initialize();
         ::atexit(Exiv2::XmpParser::terminate);
-      
-	//        #if EXIV2_TEST_VERSION(0, 27, 1)
+        #if EXIV2_TEST_VERSION(0, 28, 1)
             Exiv2::Image::AutoPtr EXimage = Exiv2::ImageFactory::open(file);
-	    //#else
-            //Exiv2::Image::UniquePtr EXimage = Exiv2::ImageFactory::open(file);
-	    //#endif
+	    #else
+            Exiv2::Image::UniquePtr EXimage = Exiv2::ImageFactory::open(file);
+	    #endif
         assert(EXimage.get() != 0);
         EXimage->readMetadata();
         Exiv2::ExifData &ed = EXimage->exifData();
